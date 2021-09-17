23252e4de700a942037e97f4ad0586f7c56c5ec5
Himanshu0894 <84079963+Himanshu0894@users.noreply.github.com>
2021-09-17 18:20:47 +0530

    Add files via upload

---
+++ B.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+       public class B {
+
+     private String str ="Hello";
+     public String publicstr ="hello";
+     
+     private void print() {
+    	
+    	 int a = 54;
+    	 int b = 47;
+    	 int sum = a+b;
+    	 System.out.println(sum);
+     }
+     public void add() {
+    	 print();
+    	 System.out.println("Addition");
+    	 
+     }
+       }
