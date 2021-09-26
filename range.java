3ef739f91b612b1448608f97fe2e17558ebc322a
Himanshu0894 <84079963+Himanshu0894@users.noreply.github.com>
2021-09-26 21:23:24 +0530

    Add files via upload

---
+++ range.java
@@ -0,0 +1,32 @@
+package primerange;
+import java.util.Scanner;
+public class range {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the first number : ");
+		int start = sc.nextInt();
+		System.out.print("Enter the second number : ");
+		int end = sc.nextInt();
+		System.out.println("List of prime numbers between " + start + "and" + end);
+		for (int i = start; i<=end;i++) {
+			if (isPrime(i)) {
+				System.out.println(i);
+			}
+		}
+
+	}
+	private static boolean isPrime(int n) {
+		if (n<=1) {
+			return false;
+		}
+		for (int i =2; i<=Math.sqrt(n);i++) {
+			if (n%i==0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+
+}
